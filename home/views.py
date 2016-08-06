a53028e5279428667acbf7ae7baebc52deb413ea
Florian Süßer <florian.suesser@macbook-pro-XXX.fritz.box>
2016-08-06 22:12:01 +0200

    now blog on home page

--- home/views.py
+++ home/views.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from home.models import Announcement
+from blog.models import BlogEntry
 # Create your views here.
 
 
 def index(request):
     announcements = Announcement.objects.order_by('-date')
+    blog_entries = BlogEntry.objects.order_by('-date')
     template = loader.get_template('home/index.html')
     context = RequestContext(request, {
         'announcements': announcements,
+        'blog_entries': blog_entries,
     })
     return HttpResponse(template.render(context))
