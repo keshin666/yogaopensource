d41e90cb993142e10c4e1e95284c4098aa0ec616
roccosportal <tadosntmas@riseup.net>
2016-08-06 22:06:57 +0200

    show only unexpired announcements

--- home/views.py
+++ home/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from home.models import Announcement
+import datetime
 # Create your views here.
 
 
 def index(request):
-    announcements = Announcement.objects.order_by('-date')
+    announcements = Announcement.objects.filter(expiry_date__gte=datetime.date.today())
     template = loader.get_template('home/index.html')
     context = RequestContext(request, {
         'announcements': announcements,
